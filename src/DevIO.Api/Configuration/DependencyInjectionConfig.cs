e2ad39584fa1c467c858012d590236cf8639b039
Gabriel Costa <gubsocosta@gmail.com>
2024-06-20 19:21:07 -0300

    wip

--- src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
+++ src/DevIO.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,5 @@
 using DevIO.Business.Interfaces;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 
@@ -10,6 +11,7 @@ public static class DependencyInjectionConfig
     {
         builder.Services.AddScoped<MeuDbContext>();
         builder.Services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+        builder.Services.AddScoped<IFornecedorService, FornecedorService>();
         
         return builder;
     }
